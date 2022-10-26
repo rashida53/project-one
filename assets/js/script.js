be6053dcdab5fd95d3b72561e4f6287b4147be07
Rashida Kapadia <rashidamk21@gmail.com>
2022-10-25 21:23:42 -0500

    add dish and ingredient search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
-var APIKey = '3b2cd8c9b12f4c8693e10ee77338f3a7';
+var APIKey = '891e9d3f9aa64c16809947ac6f8de537';
 
 var userFormEl = document.querySelector('#user-form');
 var dishName = document.querySelector('#food');
 var cuisineName = document.querySelector('#cuisine');
+
+var dishesContainer = $('#showIngredients');
+
+var ingredientsContainer = $('#ingredientsAndPrices');
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -26,14 +30,60 @@ function getDishInfo(dish, cuisine) {
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    console.log(data);
                     for (var i = 0; i < data.results.length; i++) {
                         console.log(data.results[i].title);
+                        displayDishName(data.results[i]);
                     }
-
                 })
             }
-        }
-        )
+        })
 }
+
+var displayDishName = function (dish) {
+
+    var buttonContent = dish.title;
+
+    var button = $('<button>');
+    button.text(buttonContent);
+    button.attr('id', dish.id);
+    button.on('click', clickDish);
+    dishesContainer.append(button);
+
+}
+
+function clickDish(event) {
+    event.preventDefault();
+
+    var buttonClicked = event.target;
+    displayIngredients(buttonClicked.id);
+}
+
+var displayIngredients = function (id) {
+    ingredientsContainer.empty();
+    var apiUrl = 'https://api.spoonacular.com/recipes/' + id + '/information?apiKey=' + APIKey
+
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    for (var i = 0; i < data.extendedIngredients.length; i++) {
+
+                        var listItemContent = data.extendedIngredients[i].name;
+                        var priceContent = data.extendedIngredients[i].amount + 'USD';
+
+                        var listItem = $('<li>');
+                        listItem.text(listItemContent);
+                        var priceItem = $('<li>');
+                        priceItem.text(priceContent);
+
+                        ingredientsContainer.append(listItem);
+                        ingredientsContainer.append(priceItem);
+                    }
+
+                });
+            }
+        })
+};
+
+
 
